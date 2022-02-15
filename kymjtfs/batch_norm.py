0bacc212868e88aad3a0a5441727b0375fe3bac5
Cyrus <cyrusvahidi@gmail.com>
2022-02-15 12:07:02 +0000

    fix BN

--- kymjtfs/batch_norm.py
+++ kymjtfs/batch_norm.py
@@ -7,38 +7,40 @@ class ScatteringBatchNorm(nn.Module):
         designed particularly for scattering paths.
     
     Args:
-        num_features: the number of scattering paths
+        path_shape: the shape of scattering paths and lambda (alpha x beta, lambda)
         c: an optionally learnable multiplicative featurewise constant, used
             before taking the logarithm to gaussianize the histogram of scattering 
             path values.
     '''
-    def __init__(self, num_features, c: float = None, eval_mode: bool = False):
+    def __init__(self, path_shape, c: float = None, eval_mode: bool = False):
         super().__init__()
 
-        self.num_features = num_features 
-        self.mu = torch.zeros(self.num_features)
-        self.c = torch.nn.Parameter(torch.zeros(self.num_features)) if not c else c
+        self.path_shape = path_shape
+
+        self.register_buffer("mu", torch.zeros(self.path_shape))
+        # self.c = torch.nn.Parameter(torch.ones(num_features, ) * 5.0) if not c else c
 
         self.eval_mode = eval_mode
 
     def _check_input_dim(self, x):
-        if int(x.shape[1]) != int(self.num_features):
+        if x.shape[1:3] != self.path_shape:
             raise ValueError(
-                "expected second dim to equal feature dim: {} (got {} input)".format(self.num_features, x.shape[1])
+                "expected second dim to equal feature dim: {} (got {} input)".format(self.path_shape, x.shape[1:3])
             )
+
+    def reset_stats(self):
+        pass
 
     def forward(self, sx):
         self._check_input_dim(sx)
         
-        sx = sx.permute(0, 2, 3, 1)
         if not self.eval_mode:
             batch_size = sx.shape[0]
-            batch_mu =  (1 / batch_size) * torch.sum(sx, dim=0)
+            batch_mu =  sx.mean(dim=0).mean(dim=-1)
             
             self.mu = (1 / (batch_size + 1)) * self.mu.type_as(sx) + (batch_size / (batch_size + 1)) * batch_mu
 
-        sx = torch.log1p(torch.exp(self.c) * sx / (1e-3 + self.mu))
-        sx = sx.permute(0, 3, 1, 2)
+        sx = sx / (1e-3 + self.mu.view(1, self.mu.shape[0], -1, 1))
         return sx 
 
     def eval(self):
