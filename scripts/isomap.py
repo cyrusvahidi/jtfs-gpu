47d1ada935eb1103b7117e2cfa3641b11a6f17c2
OverLordGoldDragon <16495490+OverLordGoldDragon@users.noreply.github.com>
2022-04-12 21:30:56 +0400

    iso

--- scripts/isomap.py
+++ scripts/isomap.py
@@ -51,7 +51,7 @@ def extract_mfcc(audio, f0s, fms, gammas, sr, n_mfcc = 20):
         for j, fm in enumerate(fms):
             for k, gamma in enumerate(gammas):
                 mfcc[i, j, k,:] = np.mean(librosa.feature.mfcc(y=audio[i,j,k], sr=sr), axis=-1)
-    
+
     return mfcc.reshape(-1, mfcc.shape[-1])
 
 
@@ -61,8 +61,10 @@ def extract_time_scattering(audio, duration, sr, **ts_kwargs):
                         T=N,
                         Q=1,
                         # Q=8,
-                        J=12).cuda()
-    
+                        pad_mode='zero',
+                        J=int(np.log2(N) - 1),
+                        ).cuda()
+
     X = torch.tensor(audio).cuda()
     n_samples = X.shape[0]
     n_paths = scat(X[0]).shape[0]
@@ -80,9 +82,11 @@ def extract_jtfs(audio, duration, sr, **jtfs_kwargs):
         shape=(N, ),
         T=N,
         Q=8,
-        J=12,
-        max_pad_factor=1, 
-        max_pad_factor_fr=1).cuda()
+        J=int(np.log2(N) - 1),
+        pad_mode='zero',
+        pad_mode_fr='zero',
+        max_pad_factor=3,
+        max_pad_factor_fr=3).cuda()
     X = torch.tensor(audio).cuda()
     n_samples, n_paths = X.shape[0], jtfs(X[0]).shape[1]
     sx = torch.zeros(n_samples, n_paths)
@@ -95,8 +99,8 @@ def extract_jtfs(audio, duration, sr, **jtfs_kwargs):
 
 def extract_openl3(audio, sr, **ol3_kwargs):
     X_ol3, _ = openl3.get_audio_embedding(
-        list(audio), 
-        sr, 
+        list(audio),
+        sr,
         batch_size=32,
         frontend='kapre',
         content_type='music')
@@ -107,7 +111,7 @@ def extract_strf(audio, duration, sr, **strf_kwargs):
     sys.path.insert(1, os.getcwd() + '/strf-like-model')
     import auditory
 
-    X = audio 
+    X = audio
     n_samples = X.shape[0]
     S = auditory.strf(X[0, :], audio_fs=sr, duration=duration)
     S = np.concatenate((
@@ -132,11 +136,11 @@ def plot_isomap(Y, cmap, out_dir):
     fig = plt.figure(figsize=plt.figaspect(0.5))
     ax = fig.add_subplot(1, 3, 1, projection='3d')
     ax.scatter3D(Y[:, 0], Y[:, 1], Y[:, 2], c=cmap[0], cmap='bwr');
-    
+
     ax.set_xticklabels([])
     ax.set_yticklabels([])
     ax.set_zticklabels([])
-    
+
     # f modulator
     ax = fig.add_subplot(1, 3, 2, projection='3d')
     ax.scatter3D(Y[:, 0], Y[:, 1], Y[:, 2], c=cmap[1], cmap='bwr');
@@ -176,7 +180,7 @@ def plot_knn_regression(ratios, out_dir):
             plt.title(objs[obj])
             if obj == 0:
                 plt.ylabel("Relative estimate")
-        
+
     plt.tight_layout()
     plt.savefig(os.path.join(out_dir, 'knn.png'))
 
@@ -201,20 +205,20 @@ def run_isomaps(X, cmap, out_dir, n_neighbors=40):
             np.exp(np.mean(np.log(cmap[:, knn[1][i, :]]), axis=1)) / cmap[:, i]
             for i in range(X[feat].shape[0])
         ])
-        
+
     plot_knn_regression(ratios, out_dir)
 
 
 def run_isomap(
     n_steps = 16,
-    f0_min = 512, 
+    f0_min = 512,
     f0_max = 1024,
     fm_min = 4,
     fm_max = 16,
     gamma_min = 0.5,
     gamma_max = 4,
-    bw = 2, 
-    duration = 4, 
+    bw = 2,
+    duration = 4,
     sr = 2**13,
     out_dir = '/img'):
 
@@ -227,12 +231,12 @@ def run_isomap(
     f0s = np.logspace(np.log10(f0_min), np.log10(f0_max), n_steps)
     fms = np.logspace(np.log10(fm_min), np.log10(fm_max), n_steps)
     gammas = np.logspace(np.log10(gamma_min), np.log10(gamma_max), n_steps)
-    
+
     audio, cmap = generate_audio(f0s, fms, gammas, duration, sr)
 
     # mfcc = extract_mfcc(audio, f0s, fms, gammas, sr)
     ts = extract_time_scattering(audio.reshape(-1, audio.shape[-1]), duration, sr)
-    jtfs = extract_jtfs(audio.reshape(-1, audio.shape[-1]), duration, sr)    
+    jtfs = extract_jtfs(audio.reshape(-1, audio.shape[-1]), duration, sr)
     # ol3 = extract_openl3(audio.reshape(-1, audio.shape[-1]), sr)
     # strf = extract_strf(audio.reshape(-1, audio.shape[-1]), duration, sr)
 
